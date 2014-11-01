abbf2f051de343258231676577155802516a1479
rideg <sandor.rideg@gmail.com>
2014-11-01 22:04:59 +0100

    Checkstyle added

--- src/main/java/org/jroute/channel/BorokaThreadFactory.java
+++ src/main/java/org/jroute/channel/BorokaThreadFactory.java
@@ -1,13 +1,11 @@
 package org.jroute.channel;
 
-import com.sun.istack.internal.NotNull;
-
 import java.util.concurrent.ThreadFactory;
 
 public class BorokaThreadFactory implements ThreadFactory {
 
     @Override
-    public Thread newThread(@NotNull Runnable r) {
+    public Thread newThread(Runnable r) {
         return new Thread(r, "Boroka Server Thread");
     }
 }
